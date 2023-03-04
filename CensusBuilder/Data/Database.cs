9eab5ed669bd19758c45df43c522896a4964925e
Matthew Durrant <sum0199@gmail.com>
2023-03-04 12:33:14 +0000

    remove database load code

--- CensusBuilder/Data/Database.cs
+++ CensusBuilder/Data/Database.cs
@@ -9,22 +9,6 @@ namespace CensusBuilder.Data
             AddFromText(Census1841.Data);
             AddFromText(Census1851.Data);
 
-
-
-            string[] text1851 = Census1851.Data.Split("\r\n");
-            Models.FamilySearch.CensusRecord fsRecord1851 = Models.FamilySearch.CensusRecord.FromClipboardString(text1851);
-            CensusRecord censusRecord1851 = fsRecord1851.ToCensusRecord();
-
-            Census census1841 = Census.EnglandAndWales1841;
-            census1841.Records = new() { censusRecord1841 };
-            Census census1851 = Census.EnglandAndWales1851;
-            census1851.Records = new() { censusRecord1851 };
-
-            Censuses = new()
-            {
-                census1841,
-                census1851
-            };
         }
 
         public List<Census> Censuses { get; set; } = new();
